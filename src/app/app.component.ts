deaaa86c78e7df03ec9edb93516fbd51b805ebaf
codemattcode <codemattcode@gmail.com>
2017-05-01 20:48:45 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'Busting our guts to make beautiful user experiences!';
+  lime: ['this', 'that', 'the other'];
+}
